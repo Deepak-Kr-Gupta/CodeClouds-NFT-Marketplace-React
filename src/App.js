8024894487484cae624620b222c8be241a156880
CISPL-DEEPAK <deepak.gupta@codeclouds.com>
2022-10-14 13:58:16 +0530

    code enhancement

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import contractABI from "./contractABI.json";
 //const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"; //Local
 const contractAddress = "0x0Bf04d8b3e0B860F68843D368023E89D9513f4F0"; // Production
 
-
 function App() {
  
 	const [account, setAccount] = useState(null);
@@ -15,39 +14,24 @@ function App() {
   // state for whether app is minting or not.
 	const [isMinting, setIsMinting] = useState(false);
 
-
   useEffect(() => {
-    console.log("test");
 		if (window.ethereum) {
 			setIsWalletInstalled(true);
 		}
 	}, []);
 
-  useEffect(() => {
+   useEffect(() => {
 		function initNFTContract() {
 			const provider = new ethers.providers.Web3Provider(window.ethereum);
       const signer = provider.getSigner();
       setNFTContract(new Contract(contractAddress,contractABI.abi,signer));
 		}
-		initNFTContract();
-	}, [account]);
+    if (window.ethereum) {
+			initNFTContract();
+		}
+	 }, [account]);
 
-
-  async function connectWallet() {
-		window.ethereum
-			.request({
-				method: "eth_requestAccounts",
-			})
-			.then((accounts) => {
-				setAccount(accounts[0]);
-			})
-			.catch((error) => {
-				alert("Something went wrong");
-			});
-	}
- 
-
-    const data = [
+ const data = [
         {
             url: "./assets/images/cc-nft-1.png",
             param: "handleMint('https://gateway.pinata.cloud/ipfs/QmQTSByCuj8soPZxNFgouwvjZ7LmSgrrDRfTDUvxkmHAPY/cc-nft-1.png')",
@@ -81,72 +65,67 @@ function App() {
             param: "handleMint('https://gateway.pinata.cloud/ipfs/QmQTSByCuj8soPZxNFgouwvjZ7LmSgrrDRfTDUvxkmHAPY/cc-nft-8.png')",
         },
     ];
+ 
+  async function connectWallet() {
+		window.ethereum
+			.request({
+				method: "eth_requestAccounts",
+			})
+			.then((accounts) => {
+				setAccount(accounts[0]);
+			})
+			.catch((error) => {
+				alert("Something went wrong");
+			});
+	  }
+  
+  async function handleMint(tokenURI) {
+      setIsMinting(true);
+          try {
+            const options = {value: ethers.utils.parseEther("0.00")};
+            const response = await NFTContract.mintNFT(tokenURI, options);
 
-    async function withdrawMoney(){
-        try {
-            const response = await NFTContract.withdrawMoney();
             console.log("Received: ", response);
+
           } catch (err) {
               alert(err);
           }
-    }
-    async function CountNFT(){
-      try {
-          const response = await NFTContract.balanceOf(account);
-          console.log("Add: ", account);
-          console.log(response);
-          const hexToDecimal = parseInt(response, 16);
-          console.log(hexToDecimal);
-          console.log("NFT Count: ", JSON.parse(response));
-        } catch (err) {
-            alert(err);
-        }
-        
-  }
+          finally {
+            setIsMinting(false);
+          }
+  }   
 
-    async function handleMint(tokenURI) {
-        setIsMinting(true);
-            try {
-              const options = {value: ethers.utils.parseEther("0.00")};
-              const response = await NFTContract.mintNFT(tokenURI, options);
+  // async function withdrawMoney(){
+  //     try {
+  //         const response = await NFTContract.withdrawMoney();
+  //         console.log("Received: ", response);
+  //       } catch (err) {
+  //           alert(err);
+  //       }
+  // }
 
-              console.log("Received: ", response);
-
-            } catch (err) {
-                alert(err);
-            }
-            finally {
-              setIsMinting(false);
-            }
-    }
-
-    if (account === null) {
-      return (
+  // async function CountNFT(){
+  //     try {
+  //         const response = await NFTContract.balanceOf(account);
+  //         console.log("Add: ", account);
+  //         console.log(response);
+  //         const hexToDecimal = parseInt(response, 16);
+  //         console.log(hexToDecimal);
+  //         console.log("NFT Count: ", JSON.parse(response));
+  //       } catch (err) {
+  //           alert(err);
+  //       }
+  //   }
+  return (
         <>
-         <div className="container">
-           <br/>
-          <h1>CodeClouds NFT Marketplace</h1>
-          <h2>NFT Collection</h2>
-          <p>Buy a Free NFT from our marketplace.</p>
-  
-          {isWalletInstalled ? (
-            <button onClick={connectWallet}>Connect Wallet</button>
-          ) : (
-            <p>Install Metamask wallet</p>
-          )}
-          </div>
-          </>
-      );
-    }
-
-    return (
-        <>
-            <div className="container">
+          <div className="container">
             <br/>
             <h1>CodeClouds NFT Marketplace</h1>
-          
              <h2>NFT Collection</h2>
-                {data.map((item, index) => (
+
+             {account ? (
+              <>
+              {data.map((item, index) => (
                     <div className="imgDiv">
                         <img
                             src={item.url}
@@ -164,22 +143,37 @@ function App() {
                         </button>
                     </div>
                 ))}
+                 <div>
+                  <br></br>
                  {/* <button 
                             onClick={() => {
                                 withdrawMoney();
                             }}
                         >
                             Withdraw Money from Contract
-                 </button> */}
-                 {/* <button 
+                 </button>
+                 <button 
                             onClick={() => {
                               CountNFT();
                             }}
                         >
-                            CodeClouds NFT Count
+                            CountNFT from Contract
                  </button> */}
+                 </div>
+
+              </>
+            
+          ) : (
+            <>
+            {isWalletInstalled ? (
+              <button onClick={connectWallet}>Connect Wallet</button>
+            ) : (
+              <p>Please install Metamask wallet</p>
+            )}
+            </>
+          )}  
           
-        </div>
+          </div>
 
         </>
     );
